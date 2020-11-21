fed393d6610b0877df10b0365f519bec0ff863e8
Matt-JOJO <62987746+Matt-JOJO@users.noreply.github.com>
2020-11-21 21:17:37 +0100

    playAturn

--- Game.cpp
+++ Game.cpp
@@ -12,11 +12,11 @@ void Game::utilisationDeCarteEvent(Special *Carte, GameBoard *GB, GameBoard *M_o
             GB->enleverHP(1);
             M_opponent->enleverHP(2);
 
-             /*Bombe Atomique : « six Aout 45 »
+            /*Bombe Atomique : « six Aout 45 »
 
-             Créature adverse -2 PV
+            Créature adverse -2 PV
 
-             Créature allié -1 PV*/
+            Créature allié -1 PV*/
             break;
         case 2:
             M_opponent->getFirstCard()->display();
@@ -50,11 +50,16 @@ void Game::utilisationDeCarteEvent(Special *Carte, GameBoard *GB, GameBoard *M_o
 
 Game::Game(GameBoard *GB1, GameBoard *GB2) : m_GB1{GB1}, m_GB2{GB2} {}
 
-bool Game::playATurn(GameBoard *GB,  GameBoard *GB2) {
+bool Game::playATurn(GameBoard *GB, GameBoard *GB2) {
 //le joueur 1 joue en premier
-
-
-
+    this->drawPhase(GB, GB2);
+    this->battlePhase(GB);//attaque...
+    this->verification(GB2);//on vérifie que l'autre joueur ne soit pas mort.
+    if (verification(GB2)) {//si la vérification est vraie donc le joueur 2 à été tué pendant la battlePhase
+        return false;//donc on retourne false et dans le main ça mettra fin à la partie
+    } else {
+        return true;//sinon on continue la partie et c'est au tour du joueur 2
+    }
 }
 
 //  FAIRE ATENTION AU DYNAMIQUE CASTE !!!!
@@ -93,11 +98,11 @@ void Game::battlePhase(GameBoard *GB) {
 
 }
 
-bool Game::verification(GameBoard *GB)  {
-if (GB->getPV()==0){
-    return true;
-}
-
+bool Game::verification(GameBoard *GB) {
+    if (GB->getPV() == 0) {//si les PV sont nulles alors la vérification devient vraie.
+        return true;
+    } else
+        return false;
 
 }
 
