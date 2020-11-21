0bc807542ddb0855ea85565e742ecc7e72ca5cef
Aymeric <aymeric.moulet@edu.ece.fr>
2020-11-21 10:03:37 +0100

    partie Aymeric

---
+++ Game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aymeric Ecole on 21/11/2020.
+//
+
+#include "Game.h"
---
+++ Game.h
@@ -0,0 +1,14 @@
+//
+// Created by aymeric Ecole on 21/11/2020.
+//
+
+#ifndef ECEMON_T_GAME_H
+#define ECEMON_T_GAME_H
+
+
+class Game {
+
+};
+
+
+#endif //ECEMON_T_GAME_H
